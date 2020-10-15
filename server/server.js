d0731de3a205e8eeed38c9fc001066007e62d20a
Choi702 <jaeychoi1@gmail.com>
2020-10-15 12:04:43 -0700

    server setup complete

---
+++ server/server.js
@@ -0,0 +1,25 @@
+'use strict'
+
+//Bring in our dependencies
+const express = require('express');
+const cors = require('cors');
+
+require('dotenv').config();
+
+
+//Declare our port for our server to listen on
+const PORT = process.env.PORT || 3000;
+
+// start/._nstanciate Express
+const app = express();
+
+// Use CORS (cross origin resource sharing)
+app.use(cors());
+
+
+
+//start our server
+app.listen(PORT, () => {
+  console.log(`Server is now listening on port ${PORT}`);
+});
+
