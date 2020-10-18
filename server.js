3bd40693b7e160e47282460a96156e64e5c165b0
Choi702 <jaeychoi1@gmail.com>
2020-10-17 20:00:16 -0700

    feature 2 finished

--- server.js
+++ server.js
@@ -3,60 +3,62 @@
 //Bring in our dependencies
 const express = require('express');
 const cors = require('cors');
+const superagent = require('superagent');
 
+//environmental variables
 require('dotenv').config();
 
 
 //Declare our port for our server to listen on
 const PORT = process.env.PORT || 3000;
-
-// start/._nstanciate Express
 const app = express();
-
-// Use CORS (cross origin resource sharing)
 app.use(cors());
 
 //Routes
-app.get('/', (request, response) =>{
- response.send('Whats Up Man!');
-})
+// app.get('/location', locationHandler => {
+//   response.send('Whats Up Man!');
+// });
 
 
-app.get('/location', (request, response) =>{
-let city = request.query.city;
-  //getting the data from the database or api using a flat file
-  let locationData = require('./data/location.json')[0];
-  let location = new Location(locationData, city);
-  response.send(location);
+//Route handlers
+app.get('/location', (req, res) => {
+  let city = req.query.city;
+  let key = process.env.LOCATIONIQ_API_KEY;
+console.log('key');
+  const URL = `https://us1.locationiq.com/v1/search.php?key=${key}&q=${city}&format=json`;
+  superagent.get(URL)
+    .then(data => {
+      let location = new Location(data.body[0], city);
+      res.status(200).send(location);
+    })
+    .catch(error => {
+      console.log('error', error);
+      res.status(500).send('Your API call did not work!');
+    });
+  
 });
-
-//constructor to tailor incoming raw data
-function Location(obj, query){
-  this.latitude = obj.lat;
-  this.longitude = obj.lon;
-  this.search_query = query;
-  this.formatted_query = obj.display_name;
+// constructor to tailor incoming raw data
+function Location(obj, query) {
+    this.latitude = obj.lat;
+    this.longitude = obj.lon;
+    this.search_query = query;
+    this.formatted_query = obj.display_name;
 }
- app.get('/weather', (request, response) => {
-  let data = require('./data/weather.json');
-  let weatherArray = [];
-  data.data.forEach(val => {
-    weatherArray.push(new Weather(val));  
-    
-  });
-  response.send(weatherArray);
- })
-
- function Weather(obj){
-   this.forecast = obj.weather.description;
-   this.time = new Date(obj.valid_date).toDateString();
 
 
- }
+app.get('/weather', (req, res) =>{
+let city = req.query.city;
+let key = process.env.WEATHER;
+const URL = `http://api.weatherbit.io/v2.0/forecast/daily?lat={47.61}&lon={-122.33}?city={Seattle}&country={US}`
+superagent.get(URL)
 
 
 
+
+})
+       
+   
 //start our server
 app.listen(PORT, () => {
-  console.log(`Server is now listening on port ${PORT}`);
-});
+    console.log(`Server is now listening on port ${PORT}`);
+  });
